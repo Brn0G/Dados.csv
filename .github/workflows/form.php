8b93c1e273accbf8b8272d64362cb69f182a9161
Breno Gomes <89309019+Brn0G@users.noreply.github.com>
2021-12-04 17:24:53 -0300

    form.php

---
+++ .github/workflows/form.php
@@ -0,0 +1,19 @@
+<h1> MINE HORTA VALE VERDE </h1>
+<form action="salva.php" method="post">
+    Data: <input type="text" name="data" size="6"><br>
+    Nº: <input type="text" name="n" size="6"><br>
+    Firma: <input type="text" name="firma" size="40"><br>
+    Ender: <input type="text" name="ender" size="20"><br>
+    Compl: <input type="text" name="compl" size="20"><br>
+    Bairro: <input type="text" name="bairro" size="20"><br>
+    Cidade: <input type="text" name="cidade" size="20"><br>
+    Estado: <input type="text" name="estado" size="20"><br>
+    Cep: <input type="text" name="cep" size="10"><br>
+    CNPJ: <input type="text" name="cnpj" size="20"><br>
+    Inscr: <input type="text" name="inscr" size="20"><br>
+    Tels.: <input type="text" name="tels" size="20"><br>
+    Celular: <input type="text" name="cel" size="20"><br>
+    E-mail: <input type="text" name="email" size="20"><br>
+    OBS: <input type="text" name="obs" size="50"><br>
+    <input type="submit"  value="Salvar"> <input type="reset">
+</form>
